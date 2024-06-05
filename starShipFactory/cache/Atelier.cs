027277dad1b6cb095ab1959d0a905bb0f9db52e1
julienEcole <julien.riviere94120@gmail.com>
2024-06-05 15:51:07 +0200

    feat : séparation du stock et de la production de composant

--- starShipFactory/cache/Atelier.cs
+++ starShipFactory/cache/Atelier.cs
@@ -4,42 +4,97 @@ namespace starShipFactory.cache
 {
     public static class Atelier
     {
-        static readonly Dictionary<Component, int> Stock = new Dictionary<Component, int>();
+        static readonly Dictionary<string, int> Stock = new Dictionary<string, int>();
+        static readonly Dictionary<string, int> InProduction = new Dictionary<string, int>();
 
         static Atelier()
         {
             // Initialiser le stock avec des valeurs par defaut
-            foreach (Component type in Enum.GetValues(typeof(Component)))
-            {
-                Stock[type] = 0;
-            }
         }
 
-        public static void Add(Component type, int quantity)
+        public static void AddStock(string typeName, int quantity)
         {
-            if (Stock.ContainsKey(type))
+            if (Stock.ContainsKey(typeName))
             {
-                Stock[type] += quantity;
+                Stock[typeName] += quantity;
             }
             else
             {
-                Stock[type] = quantity;
+                Stock[typeName] = quantity;
             }
         }
 
-        public static void Remove(Component type, int quantity)
+        public static bool RemoveStock(string type, int quantity)
         {
             if (Stock.ContainsKey(type))
             {
                 Stock[type] -= quantity;
                 if (Stock[type] < 0)
                 {
-                    Stock[type] = 0;
+                    Stock[type] += quantity;
+                    return false;
                 }
+                return true;
+            }
+            return false;
+        }
+
+        public static void MoveFromStockToProduction(string typeName, int quantity)
+        {
+            if (RemoveStock(typeName, quantity))
+            {
+                if (InProduction.ContainsKey(typeName))
+                {
+                    InProduction[typeName] += quantity;
+                }
+                else
+                {
+                    InProduction[typeName] = quantity;
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("Not enough stock to move to production.");
             }
         }
 
-        public static int GetQuantity(Component type)
+        public static void MoveBackToStockFromProduction(string typeName, int quantity)
+        {
+            if (InProduction.ContainsKey(typeName) && InProduction[typeName] >= quantity)
+            {
+                InProduction[typeName] -= quantity;
+                if (Stock.ContainsKey(typeName))
+                {
+                    Stock[typeName] += quantity;
+                }
+                else
+                {
+                    Stock[typeName] = quantity;
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("Not enough items in production to move back to stock.");
+            }
+        }
+
+        public static void getBackAllToStockFromProduction()
+        {
+            foreach (var item in InProduction)
+            {
+                if (Stock.ContainsKey(item.Key))
+                {
+                    Stock[item.Key] += item.Value;
+                }
+                else
+                {
+                    Stock[item.Key] = item.Value;
+                }
+            }
+            InProduction.Clear();
+        }
+
+        public static int GetQuantity(string type)
         {
             return Stock.ContainsKey(type) ? Stock[type] : 0;
         }
